ac9b004d2de8799b8cb5f020f49f9cae73c3a19d
Gary Barmak <gary.barmak@S05200-MBPR.local>
2019-10-16 14:40:37 -0500

    Add Banner vue component

---
+++ src/components/Banner.vue
@@ -0,0 +1,38 @@
+<template>
+  <section class="banner" v-bind:style="{ backgroundImage: 'url(' + imageUrl + ')' }">
+      <h2>{{ header }}</h2>
+      <p>{{ blurb }}</p>
+  </section>
+</template>
+
+<script>
+export default {
+  beforeMount() {
+    const self = this;
+    fetch('/content/dam/bayer/banner-1/jcr:content/data.infinity.json')
+    .then(function(response) {
+      return response.json();
+    })
+    .then(function(json) {
+      self.header = json.master.header;
+      self.imageUrl = json.master.image;
+      self.blurb = json.master.blurb;
+    })
+  },
+  data() {
+    return {
+      header: '',
+      blurb: '',
+      imageUrl: ''
+    };
+  },
+};
+</script>
+
+<style scoped>
+    .banner{
+      width: 1165x;
+      height: 654px;
+      /*line-height: 654px;*/
+    }
+</style>
