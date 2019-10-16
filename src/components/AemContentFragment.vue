f403729a74c8a3952352c5a66def3745915a657b
Gary Barmak <gary.barmak@S05200-MBPR.local>
2019-10-16 11:43:37 -0500

    Add AEM content fragment vue component

---
+++ src/components/AemContentFragment.vue
@@ -0,0 +1,25 @@
+<template>
+  <div v-html=content>
+      
+  </div>
+</template>
+
+<script>
+export default {
+  beforeMount() {
+    const self = this;
+    fetch('/content/we-retail/us/en/men/jcr:content/root/responsivegrid/contentfragment.model.json')
+    .then(function(response) {
+      return response.json();
+    })
+    .then(function(json) {
+      self.content = json.elements.main.value;
+    })
+  },
+  data() {
+    return {
+      content: ''
+    };
+  },
+};
+</script>
