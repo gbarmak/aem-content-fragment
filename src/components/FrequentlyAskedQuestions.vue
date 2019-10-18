b60605c18b344dfe9e5c27991b1c188dfb20cbae
Gary Barmak <gary.barmak@S05200-MBPR.local>
2019-10-18 14:20:36 -0500

    Add Frequently Asked Questions component

---
+++ src/components/FrequentlyAskedQuestions.vue
@@ -0,0 +1,35 @@
+<template>
+  <section class="faq">
+      FAQ
+  </section>
+</template>
+
+<script>
+export default {
+  beforeMount() {
+    const self = this;
+    fetch('/content/dam/bayer/banner-1/jcr:content/data.infinity.json')
+    .then(function(response) {
+      return response.json();
+    })
+    .then(function(json) {
+      self.header = json.master.header;
+      self.imageUrl = json.master.image;
+      self.blurb = json.master.blurb;
+    })
+  },
+  data() {
+    return {
+      header: '',
+      blurb: '',
+      imageUrl: ''
+    };
+  },
+};
+</script>
+
+<style scoped>
+  .fax{
+
+  }
+</style>
